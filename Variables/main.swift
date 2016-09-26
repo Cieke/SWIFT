f168fba34ad0bd4682f1447a3de492257df4d444
Nancy De Smet <ciek.eu@gmail.com>
2016-09-26 14:56:21 +0200

    My first swift

--- Variables/main.swift
+++ Variables/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+let userName = "Richard"
+print("The user is \(userName)")
 
+var firstName = "Luke"
+var surName = "SkyWalker"
+
+var fullName = firstName + " " + surName
+print ("The full name is \(fullName)")
+
+var quote = "May the force be with you"
+print(quote.lowercased())
+print(quote.uppercased())
+
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var index = alphabet.index(alphabet.startIndex, offsetBy:5)
+let first5Char = alphabet.substring(to:index)
+print (first5Char)
+
+index = alphabet.index(alphabet.endIndex, offsetBy:-4)
+let last4Characters = alphabet.substring(from:index)
+print("The last 4 characters are \(last4Characters)")
+
+let startIndex = alphabet.index(alphabet.startIndex, offsetBy:10)
+let endIndex = alphabet.index(alphabet.startIndex, offsetBy:15)
+let rangeOfCharacters = alphabet.substring(with:startIndex..<endIndex)
+print("The range of characters are \(rangeOfCharacters)")
